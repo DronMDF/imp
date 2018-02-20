4450bebc9bb5fcdc608377124f85cb3f1e0c9558
Andrey Valyaev <dron.valyaev@gmail.com>
2018-02-20 23:15:55 +0300

    #7: Непосредственные вилдкарды

--- style.sh
+++ style.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 set -ue
+set -v
 
 # Check coding style
-cppcheck -I. -Itest --enable=all --suppress=missingIncludeSystem --quiet .
-cpplint `find -name "*.cpp" -o -name "*.h"`
+cppcheck -I. -Itest --enable=all --suppress=missingIncludeSystem --quiet *.cpp test/*.cpp
+cpplint *.cpp *.h test/*.cpp
 
 #uncrustify -c uncrustify.conf --check -q `find 2out/ -name *.cpp -o -name *.h` `find test/ -name *.cpp -o -name *.h`
 
