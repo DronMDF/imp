59d887229299d37d042abc360174b3b0049aa282
Andrey Valyaev <dron.valyaev@gmail.com>
2018-02-20 23:03:08 +0300

    #7: Quote find wildcards

--- style.sh
+++ style.sh
@@ -4,7 +4,7 @@ set -ue
 
 # Check coding style
 cppcheck -I. -Itest --enable=all --suppress=missingIncludeSystem --quiet .
-cpplint `find -name *.cpp -o -name *.h`
+cpplint `find -name "*.cpp" -o -name "*.h"`
 
 #uncrustify -c uncrustify.conf --check -q `find 2out/ -name *.cpp -o -name *.h` `find test/ -name *.cpp -o -name *.h`
 
